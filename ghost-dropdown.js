64e42e47745c790204b73717038e90e3ac4e780d
Akash M <31040302+akashmdiu@users.noreply.github.com>
2021-07-27 10:36:52 +0600

    add options functionality  + fix append issue + fix menu flash issue

--- ghost-dropdown.js
+++ ghost-dropdown.js
@@ -1,26 +1,39 @@
 (function($) {
     "use strict";
 
-    function removeOpacity() {
-        $('ul').css("opacity", "1");
-    }
-    setTimeout(removeOpacity, 1000);
 
-    function ghost_dropdown(tElement) {
+    function ghost_dropdown(options) {
 
-        // Target Element
-        const targetElement = "ul li";
-        if (tElement) {
-            targetElement = tElement;
+       let defultOptions = {
+            targetElement: "ul li",
+            hasChildrenClasses: "menu-item-has-children",
+            hasChildrenIcon: "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-down' viewBox='0 0 16 16'><path d='M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z'/></svg>",
+            hasChildDetectText: "[has_child]",
+            submenuUlClasses: "ghost-submenu",
+            subitemDetectText: "[subitem]",
+            subitemLiClasses: "subitem"
+    
         }
 
+        options = {
+            ...defultOptions,
+            ...options
+        }
+
+        console.log(options);
+
+        // Target Element
+        let targetElement = options.targetElement;
+        
         //Default value 
-        const hasChildrenClasses = "menu-item-has-children";
-        const hasChildrenIcon = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-caret-down' viewBox='0 0 16 16'><path d='M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z'/></svg>";
-        const hasChildDetectText = "[has_child]";
-        const submenuUlClasses = "ghost-submenu";
-        const subitemDetectText = "[subitem]";
-        const subitemLiClasses = "subitem";
+        let hasChildrenClasses = options.hasChildrenClasses;
+        let hasChildrenIcon = options.hasChildrenIcon;
+        let hasChildDetectText = options.hasChildDetectText;
+        let submenuUlClasses = options.submenuUlClasses;
+        let subitemDetectText = options.subitemDetectText;
+        let subitemLiClasses = options.subitemLiClasses;
+
+
 
 
 
@@ -34,6 +47,8 @@
         let elIndex = 0;
         let parentIndex = [];
 
+        $(`${targetElement}`).parent().addClass('ghost-dropdown-menu');
+
 
         // Find Dropdown parent element
         parentEl.each(function(index, element) {
@@ -46,6 +61,7 @@
 
                 $(this).append(`<ul class='${submenuUlClasses}'></ul>`); // Append submenu element
 
+                $(targetElement).css("opacity", "1");
 
             }
         });
@@ -63,21 +79,23 @@
 
                 if (subitem) {
 
-                    if (elIndex < parentIndex[i + 1] || elIndex >= parentIndex[parentIndex.length - 1]) {
+                    if (elIndex >= parentIndex[i + 1]) { // Each loop will be break
+                        return false; //Stoped each loop 
+                    }
+
+                    if (elIndex <= parentIndex[i + 1] || elIndex >= parentIndex[parentIndex.length - 1]) {
 
                         if (!indexPush.includes(index)) { //Check if not index already insert 
                             $(this).addClass(subitemLiClasses); // Add class in subitem element
                             let st = $(this).children().text(); // Find subitem inner text
                             $(this).children().text(st.replaceAll(subitemDetectText, "")); // Replace subitem inner text
+
                             domArrayElement.push(element); // Incert subitem element in dom array
                             indexPush.push(index); // incert subitem index in indexPush array
+
                         }
                     }
-
-
-                    if (elIndex + 1 >= parentIndex[i + 1]) { // Each loop will be break
-                        return false; // Each loop stoped
-                    }
+                    
                 }
                 elIndex++; // increase element index value
             });
@@ -85,23 +103,24 @@
 
             $(`.${hasChildrenClasses} ul.${submenuUlClasses}:eq(${i})`).append(domArrayElement); // Append related subitem dom element into submenu 
 
+            // console.log(domArrayElement);
             domArrayElement = []; // Make dom array element empty. 
 
         }
 
         const hasChildEl = $(`.${hasChildrenClasses}`);
         hasChildEl.each(function() {
-            if ($(this).find("> a").text().includes(hasChildDetectText)) {
-                let hasChildText = $(this).find("> a").text(); // Find has child inner text
-                $(this).find("> a").text(hasChildText.replaceAll(hasChildDetectText, ""));
+            if ($(this).find("> a:first").text().includes(hasChildDetectText)) {
+                console.log($(this).find("> a").text());
+                let hasChildText = $(this).find("> a:first").text(); // Find has child inner text
+                $(this).find("> a:first").text(hasChildText.replaceAll(hasChildDetectText, ""));
             }
         });
-
 
     }
 
     $(document).ready(function() {
-        ghost_dropdown()
+        ghost_dropdown();
     });
 
 
